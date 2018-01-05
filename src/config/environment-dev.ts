49b9721cb8e39c310d3d2053b60cf0d5b06861c8
Tafarel Chicotti <chicotti.tafarel@progam.com.br>
2018-01-05 11:00:18 -0200

    Versão 0.3.1 configuração

--- src/config/environment-dev.ts
+++ src/config/environment-dev.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-    URL_BASE: 'http://192.168.1.3/mitra/',
+    URL_BASE: 'http://192.168.1.166/central/',
     API_URI: 'sorocabaWebService/index.php',
     FILES_URI: 'central/modulos/atendimento/arquivos/',
 };
